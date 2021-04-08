206008f693bab3e695c48bb98e5b430bab4ccbde
Tanisha Bisht <agamyatani@gmail.com>
2021-04-09 01:40:12 +0530

    BUG: url rectified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   return (
-    <BrowserRouter basename='/'>
+    <BrowserRouter basename='/Jayvardhan-Portfolio'>
       <div className="app">
         <Navbar/>
         <Switch>
