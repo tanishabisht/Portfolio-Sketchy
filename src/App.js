fcb6058ea2da7ee4f35a2ca8c3bb76cd2ee07bb2
Tanisha Bisht <agamyatani@gmail.com>
2021-04-09 01:36:16 +0530

    jay porfolio V1.0

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
+import {Home, Projects, Initiatives} from './Pages'
+import {Navbar, SideLinks, Footer} from './Components'
+import './App.css'
+
+function App() {
+  return (
+    <BrowserRouter basename='/'>
+      <div className="app">
+        <Navbar/>
+        <Switch>
+          <Route path='/home' exact component={Home} />
+          <Redirect from='/' to='/home' exact/>
+          <Route path='/projects' exact component={Projects} />
+          <Route path='/initiatives' exact component={Initiatives}/>
+        </Switch>
+        <Footer/>
+        <SideLinks fb='#' insta='#' linkedIn='#' medium='#' twitter='#' utube='#' />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App;
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './footer.scss'
+
+const Footer = () => {
+  return (
+    <div className='footer_container'>
+        <p className='footer_text text-center'>DESIGNED AND BUILT BY TANISHA</p>
+    </div>
+  )
+}
+
+export default Footer
